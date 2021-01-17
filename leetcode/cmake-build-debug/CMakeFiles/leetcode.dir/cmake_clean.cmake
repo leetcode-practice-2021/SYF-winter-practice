f36ba3db013eba8781e91f0b2d19e6b2e8e63aff
Frank Shi <2396914751@qq.com>
2021-01-17 23:34:59 +0800

    215 copy

---
+++ leetcode/cmake-build-debug/CMakeFiles/leetcode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode.dir/215.cpp.o"
+  "CMakeFiles/leetcode.dir/main.cpp.o"
+  "leetcode"
+  "leetcode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ leetcode/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "215.cpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    run215();
+    return 0;
+}
